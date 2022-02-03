0ef612302fb2a36c9328bf6fa445448a37b3f406
Alexy Araujo <96698163+a-gva@users.noreply.github.com>
2022-02-03 13:26:23 -0300

    Add files via upload

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+    return (
+        <Card>
+            <div className="about">
+                <h1>About This Project</h1>
+                <p>React project</p>
+                <p>Version. 1.0.0</p>
+                <p>
+                    <a href="/">Back to Home</a>
+                </p>
+            </div>
+            About Page
+        </Card>
+    )
+}
+
+export default AboutPage;
